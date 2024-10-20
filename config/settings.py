c441b2a8eb469c1fec94b9f3fc31042d557a2b9e
Anton Gromov <seed14112@yandex.ru>
2024-10-20 22:22:50 +0300

    Добавление CORS в проект

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "corsheaders",
 
     "announcements",
     "users",
@@ -36,7 +37,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = (
+    "http://localhost:8000",
+)
 
 ROOT_URLCONF = "config.urls"
 
