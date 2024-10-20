5ce245de160483c368e0f0c1c3e15a130a393843
Anton Gromov <seed14112@yandex.ru>
2024-10-20 18:35:10 +0300

    Добавление сериализатора, представления и эндпоинта для модели User

---
+++ users/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели User
+    """
+
+    class Meta:
+
+        model = User
+        fields = ("id", "email", "first_name", "last_name", "role", "image")
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    Контроллер для модели User
+    """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
