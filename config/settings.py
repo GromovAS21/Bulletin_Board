38bb4c3c0e4ff874ddaba96b3806459028d5c077
Anton Gromov <seed14112@yandex.ru>
2024-10-20 18:05:38 +0300

    Добавление модели User и внесение соответствующих настроек

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,6 +97,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR / "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 
@@ -6,4 +7,55 @@ class User(AbstractUser):
     """
     Модель пользователя
     """
-    pass
+
+    username = None
+
+    first_name = models.CharField(
+        max_length=50,
+        verbose_name="Имя",
+        blank=True,
+        null=True
+    )
+    last_name = models.CharField(
+        max_length=50,
+        verbose_name="Фамилия",
+        blank=True,
+        null=True
+    )
+    phone = models.CharField(
+        max_length=11,
+        validators=[MinLengthValidator(11)],
+        verbose_name="Телефон",
+        blank = True,
+        null = True
+    )
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Email",
+    )
+    ROLE_CHOICES = [
+        ("admin", "Администратор"),
+        ("user", "Пользователь"),
+    ]
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default="user"
+    )
+    image = models.ImageField(
+        upload_to="users/",
+        verbose_name="Аватар пользователя",
+        blank=True,
+        null=True
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ("id",)
+
+    def __str__(self):
+        return self.email
