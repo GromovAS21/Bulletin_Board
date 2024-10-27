45e6ab69a321428e074a8325f8c54489d081177f
Anton Gromov <seed14112@yandex.ru>
2024-10-27 22:02:45 +0300

    Добавление замена параметров полей goods, amount

--- baskets/models.py
+++ baskets/models.py
@@ -9,7 +9,7 @@ class Basket(models.Model):
     Модель корзины
     """
 
-    user = models.ForeignKey(
+    author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         verbose_name="Пользователь",
@@ -19,11 +19,15 @@ class Basket(models.Model):
     goods = models.ManyToManyField(
         Announcement,
         verbose_name="Товары",
+        blank=True,
+        null=True
     )
 
     amount = models.PositiveIntegerField(
         verbose_name="Сумма к оплате",
-        default=0
+        default=0,
+        blank=True,
+        null=True
     )
 
     class Meta:
