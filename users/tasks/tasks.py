650887407e188ad537358d23401619da62e4a0aa
Антон Громов <163427927+GromovAS21@users.noreply.github.com>
2024-11-17 14:08:09 +0300

    Update tasks.py
    !

--- users/tasks/tasks.py
+++ users/tasks/tasks.py
@@ -12,7 +12,7 @@ from users.models import User
 @shared_task
 def check_birthday_users():
     """
-    Проверка дней рождения пользователей и отправка письма с поздравлением
+    Проверка дней рождения пользователей и отправка письма с поздравлением!
     """
 
     current_date = datetime.now().date()
